f039475174dd1f5790cdd64ab96d40f63b8d1db4
HimanshiZalariya <23031701055@darshan.ac.in>
2025-06-23 22:30:23 +0530

    Add files via upload

---
+++ Addition.cs
@@ -0,0 +1,21 @@
+﻿//1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Addition
+    {
+        public int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public float Add(float x, float y)
+        {
+            return x + y;
+        }
+    }
+}
