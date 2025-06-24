2dfc589b2547560659ee22f45d44ffce53a96e7d
HimanshiZalariya <23031701055@darshan.ac.in>
2025-06-24 07:58:23 +0530

    Delete git.net directory

--- git.net/Lab1_3.cs
+++
@@ -1,8 +0,0 @@
-﻿//---------------------3----------------------
-
-
-Console.WriteLine("Enter Name : ");
-string name = Convert.ToString(Console.ReadLine());
-Console.WriteLine("Enter countery : ");
-string country = Convert.ToString(Console.ReadLine());
-Console.WriteLine($"hello {name} from countey {country}");
--- git.net/Lab1_4.cs
+++
@@ -1,9 +0,0 @@
-﻿//---------------------4----------------------
-
-
-Console.Write("Enter L: ");
-int l = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter  W: ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Area : " + (l * b));
-
